423cd62a7786bee1ceccbfb0a64bb980505001f0
DANNIEL <donghoon149@gmail.com>
2021-08-13 18:09:39 +0900

    Add: 두개 뽑아서 더하기

--- src/main/java/prgms/두개_뽑아서_더하기/Solution.java
+++ src/main/java/prgms/두개_뽑아서_더하기/Solution.java
@@ -3,6 +3,7 @@ package prgms.두개_뽑아서_더하기;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
 
 
 /*
@@ -11,14 +12,30 @@ numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아
 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
  */
 class Solution {
+
+    Set<Integer> resultSet = new HashSet<>();
+
     public int[] solution(int[] numbers) {
-        int[] answer = {};
-        return answer;
+        addResultToSet(numbers);
+        List<Integer> resultList = new ArrayList<>(resultSet);
+        return resultList.stream().sorted().mapToInt(i -> i).toArray();
+    }
+
+    void addResultToSet(int[] numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length - 1; i++) {
+            sum += numbers[i];
+            for (int j = i + 1; j < numbers.length; j++) {
+                sum += numbers[j];
+                resultSet.add(sum);
+                sum -= numbers[j];
+            }
+            sum -= numbers[i];
+        }
     }
 
     @Test
     void test() {
-//        Assertions.assertEquals(43, solution(13, 17));
-//        Assertions.assertEquals(52, solution(24, 27));
+        Assertions.assertArrayEquals(new int[]{2, 3, 4, 5, 6, 7}, solution(new int[]{2, 1, 3, 4, 1}));
     }
 }
